14635eff4aebb601c33036d55d457a7c05e5b1da
mli <mli@student.42.fr>
2020-06-09 00:24:07 +0200

    Not using assert() anymore

--- main.c
+++ main.c
@@ -1,51 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/24 21:00:58 by mli               #+#    #+#             */
-/*   Updated: 2020/05/20 16:10:36 by mli              ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libasm.h"
-// int		printf(const char *str, ...);
-
-void	ft_wr_len(char *str)
-{
-	printf("The sentence tested : [%s] | len : [%lu]\n", str, strlen(str));
-	printf("\nWrite [%lu]\n", ft_write(1, str, strlen(str)));
-	printf("Strlen [%lu]\n\n", ft_strlen(str));
-}
-
 void	ft_test_strcpy(char *s2)
 {
 	char s1[1024];
 
-	memset(s1, '-', 1024);
+	memset(s1, '-', 1023);
+	s1[1023] = '\0';
 	printf("%s\n", ft_strcpy(s1, s2));
-}
-
-void	ft_test_strcmp(char *s1, char *s2)
-{
-	printf("Mine %d\n", ft_strcmp(s1, s2));
-	printf("True %d\n", strcmp(s1, s2));
-}
-
-void	ft_test_read(int fd, int count)
-{
-	char buff[1024];
-
-	memset(buff, 0, 1024);
-	printf("%d\n", (int)ft_read(fd, buff, count));
-	printf("%s\n", buff);
-}
-
-void	ft_test_strdup(char *s1)
-{
-	printf("%s\n%s\n", s1, ft_strdup(s1));
 }
 
 int		main(void)
@@ -60,10 +19,6 @@ int		main(void)
 //	ft_test_strcmp("12345", "12346");
 //	ft_test_strcmp("", "");
 //	ft_test_strcmp("12349", "12346");
-
-//	ft_test_read(open("main.c", O_RDONLY), 5);
-
-//	ft_test_strdup("123456789ABCDEF");
 
 	return (0);
 }
--- test.c
+++ test.c
@@ -8,8 +8,9 @@
 #define MAKEITABORT 0
 // Globals
 int g_all = 26, g_wr_all = 12, g_rd_all = 14;
-int g_done = 0, g_wr = 0, g_rd = 0;
+int g_done = 0, g_fail = 0, g_wr = 0, g_rd = 0;
 int wrfd = 0, rdfd = 0, rdfd_true = 0;
+char	err_buff[1024];
 
 void	__attribute__((destructor)) ft_destructor();
 
@@ -22,14 +23,13 @@ void	done_on_all(char *str, int done, int all)
 {
 	char *color = (done == all ? "\e[0;92m" : "\e[0;91m");
 
-	printf("\e[0;96m%s: %s%d\e[0;1m/\e[92m%d\e[0m\n", str, color, done, all);
+	printf("\e[0;1;96m%s: %s%d\e[0;1m/\e[92m%d\e[0m\n", str, color, done, all);
 }
 
 void	ft_destructor(void)
 {
-	printf("\n");
-	printf("\033[0;94mTests finished!\n");
-	done_on_all("Total", g_done, g_all);
+	printf("\t\t\033[0;1;91mTESTS FINISHED!\n");
+	done_on_all("Total", g_done - g_fail, g_all);
 	done_on_all("ft_write", g_wr, g_wr_all);
 	done_on_all("ft_read", g_rd, g_rd_all);
 
@@ -44,84 +44,121 @@ void	ft_signal(int sign)
 	exit(1);
 }
 
-void	truefalse(const int eq[2])
+char	*tf_str(const int eq)
 {
+	return (eq ? "\e[0;92mTrue\e[0m" : "\e[0;91mFalse\e[0m");
+}
+
+void	truefalse(const int all_good, const int ret[2], const int err[2])
+{
+	g_done++;
+	if (all_good) return ; g_fail++;
+
+	int ret_eq = ret[0] == ret[1];
+	int err_eq = err[0] == err[1];
 	printf("\033[0;93m[%03d] " \
-			"\033[0;95mReturn \033[0;%s " \
-			"\033[0;94mErrno \033[0;%s\033[0m\n", ++g_done,
-			eq[0] ? "92mTrue" : "91mFalse", eq[1] ? "92mTrue" : "91mFalse");
+			"\033[0;95mReturn %s " \
+			"\033[0;94mErrno %s : %s\n",
+			g_done, tf_str(ret_eq), tf_str(err_eq), err_buff);
+	if (!ret_eq)
+		printf("Returned \e[91m%d\t\e[mExpected \e[92m%d\e[m\n", ret[0], ret[1]);
+	if (!err_eq)
+		printf("Errno is \e[91m%d\t\e[mExpected \e[92m%d\e[m\n", err[0], err[1]);
+	printf("\n");
+}
+
+char	*which_fd(int fd)
+{
+	if (fd == 0) return ("stdin");
+	if (fd == 1) return ("stdout");
+	if (fd == 2) return ("stderr");
+	if (fd == rdfd) return ("rdfd");
+	if (fd == wrfd) return ("wrfd");
+	return ("fakefd");
 }
 
 int		test_write(const int fd, const char * const str, const int size)
 {
-	int err;
 	int ret[2];
 	int eq[2];
+	int err[2];
+	int all_good;
 
-	errno = 0;
+	errno = 0; bzero(err_buff, sizeof(err_buff));
 	ret[0] = ft_write(fd, str, size);
-	err = errno;
+	err[0] = errno; errno = 0;
 	ret[1] = write(fd, str, size);
-	eq[0] = ret[0] == ret[1]; eq[1] = err == errno;
-	truefalse(eq);
-	g_wr++;
-	return (eq[0] && eq[1] ? 1 : 0);
+	err[1] = errno;
+	eq[0] = (ret[0] == ret[1]); eq[1] = (err[0] == err[1]);
+	if ((all_good = (eq[0] && eq[1] ? 1 : 0)))
+		g_wr++;
+	else
+		sprintf(err_buff, "ft_write(%d[%s], '%s', %d)",
+				fd, which_fd(fd), str, size);
+	truefalse(all_good, ret, err);
+	return (all_good);
 }
 
 int		test_read(const int fd_me, const int fd_true, const int size)
 {
 	char buf[2][1024];
-	int err;
+	int err[2];
 	int ret[2];
 	int eq[2];
+	int all_good;
 
-	errno = 0;
+	errno = 0; bzero(err_buff, sizeof(err_buff));
 	bzero(buf[0], sizeof(buf[0]));
 	bzero(buf[1], sizeof(buf[1]));
 	ret[0] = ft_read(fd_me, buf[0], size);
-	err = errno;
+	err[0] = errno; errno = 0;
 	ret[1] = read(fd_true, buf[1], size);
-	eq[0] = (ret[0] == ret[1]); eq[1] = (err == errno);
-	truefalse(eq);
-	g_rd++;
-	return (eq[0] && eq[1] && !strcmp(buf[0], buf[1]) ? 1 : 0);
+	err[1] = errno;
+	eq[0] = (ret[0] == ret[1]); eq[1] = (err[0] == err[1]);
+	if ((all_good = (eq[0] && eq[1] && !strcmp(buf[0], buf[1]) ? 1 : 0)))
+		g_rd++;
+	else
+		sprintf(err_buff, "ft_read(%d[%s], buff_eq:%s, %d)",
+			fd_me, which_fd(fd_me), tf_str(all_good), size);
+	truefalse(all_good, ret, err);
+	return (all_good);
 }
 
 void	wr_tests(void)
 {
-	assert(test_write(9, "Hello", 5));
-	assert(test_write(wrfd, "Hello\n", 6));
-	assert(test_write(wrfd, NULL, 5));
-	assert(test_write(-1, NULL, 5));
-	assert(test_write(wrfd, "ABCEDFGEFGH", 9));
-	assert(test_write(9, "ABCEDFGEFGH", 9));
-	assert(test_write(wrfd, NULL, -1));
-	assert(test_write(wrfd, "A longer sentence\n", 18));
+	test_write(9, "Hello", 5);
+	test_write(wrfd, "Hello!", 6);
+	test_write(wrfd, NULL, 5);
+	test_write(-1, NULL, 5);
+	test_write(wrfd, "ABCEDFGEFGH", 9);
+	test_write(9, "ABCEDFGEFGH", 9);
+	test_write(wrfd, NULL, -1);
+	test_write(wrfd, "A longer sentence!", 18);
 
-	assert(test_write(rdfd, "Hello\n", 6));
-	assert(test_write(rdfd, NULL, 5));
-	assert(test_write(rdfd, NULL, -1));
-	assert(test_write(rdfd, "A longer sentence\n", 18));
+	test_write(rdfd, "Hello!", 6);
+	test_write(rdfd, NULL, 5);
+	test_write(rdfd, NULL, -1);
+	test_write(rdfd, "A longer sentence!", 18);
 }
 
 void	rd_tests(void)
 {
-	assert(test_read(9, 9, 5));
-	assert(test_read(9, 9, 9));
+	test_read(9, 9, 5);
+	test_read(9, 9, 9);
 
-	assert(test_read(rdfd, rdfd_true, 5));
-	assert(test_read(rdfd, rdfd_true, 1));
-	assert(test_read(rdfd, rdfd_true, 9));
-	assert(test_read(rdfd, rdfd_true, 7));
-	assert(test_read(rdfd, rdfd_true, 18));
-	assert(test_read(rdfd, rdfd_true, 30));
+	test_read(rdfd, rdfd_true, 5);
+	test_read(rdfd, rdfd_true, 1);
+	test_read(rdfd, rdfd_true, 9);
+	test_read(rdfd, rdfd_true, 7);
+	test_read(rdfd, rdfd_true, 18);
+	test_read(rdfd, rdfd_true, 30);
 
-	assert(test_read(wrfd, wrfd, 5));
-	assert(test_read(wrfd, wrfd, 1));
-	assert(test_read(wrfd, wrfd, 9));
-	assert(test_read(wrfd, wrfd, 7));
-	assert(test_read(wrfd, wrfd, 18));
-	assert(test_read(wrfd, wrfd, 30));
+	test_read(wrfd, wrfd, 5);
+	test_read(wrfd, wrfd, 1);
+	test_read(wrfd, wrfd, 9);
+	test_read(wrfd, wrfd, 7);
+	test_read(wrfd, wrfd, 18);
+	test_read(wrfd, wrfd, 30);
 }
 
 static int	init(void)
@@ -143,8 +180,8 @@ int		main(void)
 		perror("Couldn't init tester");
 		exit(1);
 	}
-	if (!MAKEITABORT)
-		signal(SIGABRT, ft_signal);
+//	if (!MAKEITABORT)
+//		signal(SIGABRT, ft_signal);
 	wr_tests();
 	rd_tests();
 }
